f5d0d3a5d6a884ce0d99909e9cba99a102548b33
naiveprogramming <63132486+naiveprogramming@users.noreply.github.com>
2020-04-05 12:38:09 -0300

    Add files via upload

---
+++ tp1.R
@@ -0,0 +1,45 @@
+#(1)
+altas <- read.csv("C:/Users/User three/Desktop/Fede/MiM+Analytics/Materias/Modulo 1/Metodos Estadisticos Aplicados a Negocios/TP/usuarios-ecobici-2018.csv",
+                   header = TRUE, sep = ",")
+
+summary(altas)
+dim(altas)
+prop.table(table(altas$usuario_sexo))
+#Vemos que en el 2018 se dieron de alta 56182 usuarios,
+#de los cuales aproximadamente el 46% es femenino y el 54% masculino.
+#El dia donde mas altas se dieron fue el 08/10/2018.
+#La hora en la cual más usuarios se dan de alta es a las 5:57:45.
+#La edad promedio de los usuarios es de 33 años (ojo los extremos aca, ver).
+
+recorridos <- read.csv("C:/Users/User three/Desktop/Fede/MiM+Analytics/Materias/Modulo 1/Metodos Estadisticos Aplicados a Negocios/TP/recorridos-realizados-2018.csv",
+                       header = TRUE, sep = ",")
+
+#Creo una columna que indique el dia de la semana en el que se retiro la bici.
+recorridos$dia_semana <- as.factor(strftime(recorridos$fecha_origen_recorrido, "%A"))
+
+summary(recorridos)
+dim(recorridos)
+prop.table(table(recorridos$genero_usuario))
+prop.table(table(recorridos$dia_semana))
+#Vemos que en el 2018 se hicieron 2619968 recorridos, que tanto el origen
+#como el destino más comun se da en la Facultad de Medicina y que las mujeres
+#hicieron aproximadamente el 28% de los recorridos, mientras que los hombres
+#el 72%. El día de la semana en el que mas bicis son retiradas es el miercoles.
+
+#(2)
+recorridos$fecha_origen_auxiliar <- as.factor(strptime(recorridos$fecha_origen_recorrido,
+                                                                  format = "%Y-%m-%d",
+                                                                  tz = "America/Argentina/Buenos_Aires"))
+library("dplyr")
+usuarios_diarios_año <- recorridos %>% group_by(fecha_origen_auxiliar) %>% summarise(total = length(dia_semana)) %>% ungroup()
+usuarios_diarios_año$total <- as.numeric(usuarios_diarios_año$total)
+usuarios_diarios_año$dia_semana <- as.factor(strftime(usuarios_diarios_año$fecha_origen_auxiliar, "%A"))
+tab <- usuarios_diarios_año %>% group_by(dia_semana) %>% summarise(mu = mean(total)) %>% ungroup()
+tab$z <- usuarios_diarios_año %>% group_by(dia_semana) %>% summarise(mu = qt(0.975, length(total) - 1)) %>% ungroup()
+tab$std <- usuarios_diarios_año %>% group_by(dia_semana) %>% summarise(mu = sd(total)/sqrt(length(total))) %>% ungroup()
+tab$lim_inf <- tab$mu - (tab$z$mu*tab$std$mu)
+tab$lim_sup <- tab$mu + (tab$z$mu*tab$std$mu)
+
+library("ggplot2")
+tab$dia_semana <- factor(tab$dia_semana, levels = c("lunes","martes","miércoles","jueves","viernes","sábado","domingo"))
+ggplot(data = tab) + geom_pointrange(mapping = aes(x = dia_semana, y = mu, ymin = lim_inf, ymax = lim_sup)) + geom_errorbar(mapping = aes(x = dia_semana, ymin = lim_inf, ymax = lim_sup))
